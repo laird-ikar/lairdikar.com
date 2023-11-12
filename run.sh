f53786586a46c4f7464a7673ce8fc932b76d990f
laird-ikar <bguyot@student.42mulhouse.fr>
2023-11-12 12:08:44 +0100

    run: change log file path

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
-echo
-date >> .logs
+echo >> /var/log/lairdikar.com.log
+date >> /var/log/lairdikar.com.log
+pwd >> /var/log/lairdikar.com.log
 if git pull | grep -q 'Already up to date.'; then
-	echo "No changes." >> .logs
+	echo "No changes." >> /var/log/lairdikar.com.log
 else
-	echo "Changes found." >> .logs
-	echo "Restarting..." >> .logs
+	echo "Changes found." >> /var/log/lairdikar.com.log
+	echo "Restarting..." >> /var/log/lairdikar.com.log
 	docker compose -f /home/pi/Server/docker-compose.yml up --build -d
 fi
-echo "Done." >> .logs
+echo "Done." >> /var/log/lairdikar.com.log
