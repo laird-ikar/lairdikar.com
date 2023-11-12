e5ee83e04ef24fac971f6c8e673a22c907c3e206
laird-ikar <bguyot@student.42mulhouse.fr>
2023-11-12 11:53:26 +0100

    run: add logs in a file

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
+echo
+date >> .logs
 if git pull | grep -q 'Already up to date.'; then
-	echo "No changes."
+	echo "No changes." >> .logs
 else
-	echo "Changes found."
-	echo "Restarting..."
+	echo "Changes found." >> .logs
+	echo "Restarting..." >> .logs
 	docker compose -f /home/pi/Server/docker-compose.yml up --build -d
 fi
-echo "Done."
+echo "Done." >> .logs
