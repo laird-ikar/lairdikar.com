f98cc20672d342fd6d7c3d848e2eb048505958ca
laird-ikar <bguyot@student.42mulhouse.fr>
2023-11-12 11:22:53 +0100

    Add Git pull check and Docker Compose restart in run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+if git pull | grep -q 'Already up-to-date.'; then
+	echo "No changes."
+else
+	echo "Changes found."
+	echo "Restarting..."
+	docker compose -f /home/pi/Server/docker-compose.yml up --build -d
+fi
+echo "Done."
