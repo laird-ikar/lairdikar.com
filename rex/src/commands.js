de18688ece1f7c0a4bfcb5103ffc8663882ef895
laird-ikar <bguyot@student.42mulhouse.fr>
2023-12-16 16:44:23 +0100

    export commands

--- rex/src/commands.js
+++ rex/src/commands.js
@@ -1,6 +1,3 @@
-import "dotenv/config";
-import axios from "axios";
-
 const command_types = {
 	CHAT_INPUT: 1,
 	USER: 2,
@@ -24,21 +21,5 @@ const commands = Object.values(command_list).map((command) => {
 	};
 });
 
-axios
-	.post(
-		`https://discord.com/api/v10/applications/${process.env.REX_APP_ID}/commands`,
-		commands,
-		{
-			headers: {
-				Authorization: `Bot ${process.env.REX_BOT_TOKEN}`,
-			},
-		}
-	)
-	.then((res) => {
-		console.log(res.data);
-	})
-	.catch((err) => {
-		console.error(err);
-	});
-
 export { command_list };
+export default commands;
