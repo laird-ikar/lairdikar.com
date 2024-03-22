f7afd949637c4286be5d4cf8a200ed3ec3505173
Laird Ikar Azphæl <bguyot@student.42mulhouse.fr>
2024-03-22 22:11:00 +0100

    better run sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ logfile=/home/laird_ikar/lairdikar.com.log
 echo >> $logfile
 date >> $logfile
 pwd >> $logfile
-if git pull | grep -q 'Already up to date.'; then
+if git pull | grep -q 'Already up to date.' && docker ps | grep -q 'nginx' ;  then
 	echo "No changes." >> $logfile
 else
 	echo "Changes found." >> $logfile
