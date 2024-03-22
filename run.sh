cadb8112885b1a31377a5f656cd33230a1418777
laird-ikar <bguyot@student.42mulhouse.fr>
2024-03-22 22:08:21 +0100

    run.sh: move the path into ~/lairdikar.com bc im ✨lazy✨

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-cd /home/laird_ikar/Documents/lairdikar.com
+cd /home/laird_ikar/lairdikar.com
 logfile=/home/laird_ikar/lairdikar.com.log
 echo >> $logfile
 date >> $logfile
@@ -8,6 +8,6 @@ if git pull --recurse-submodules | grep -q 'Already up to date.'; then
 else
 	echo "Changes found." >> $logfile
 	echo "Restarting..." >> $logfile
-	docker compose -f /home/laird_ikar/Documents/lairdikar.com/docker-compose.yml up --build -d
+	docker compose -f /home/laird_ikar/lairdikar.com/docker-compose.yml up --build -d
 fi
 echo "Done." >> $logfile
